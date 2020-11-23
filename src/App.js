119956dd00b92fc79ecb7eae21fe4f5de0c26e31
Dharmit Dosani <dharmitdosani@gmail.com>
2020-11-24 00:00:38 +0530

    button click logic sorted

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const StarsDisplay = props => (
 const PlayNumber = props => (
   <button
     className="number"
-    onClick={() => console.log(props.number)}
+    onClick={() => props.onClick(props.number, props.status)}
     style={{backgroundColor: colors[props.status]}}
   >
     {props.number}
@@ -36,6 +36,26 @@ const App = (props) => {
     return 'available';
   };
 
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === 'used') {
+      return;
+    }
+    const newCandidateNums = 
+      currentStatus === 'available'
+      ? candidateNums.concat(number)
+      : candidateNums.filter(cn => cn !== number);
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
   return (
     <div className="game">
       <div className="help">
@@ -51,6 +71,7 @@ const App = (props) => {
               key={number}
               number={number}
               status={numberStatus(number)}
+              onClick={onNumberClick}
             />
           )}
         </div>
